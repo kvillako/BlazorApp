05aee568b5b13634c925fbfa165ea4ae9b6146e9
kvillako <katlinvillako@gmail.com>
2021-03-29 14:56:07 +0300

    Part 20. Search in ASP NET Core REST API

--- BlazorTutorial/EmployeeManagement.Api/Models/EmployeeRepository.cs
+++ BlazorTutorial/EmployeeManagement.Api/Models/EmployeeRepository.cs
@@ -77,5 +77,23 @@ namespace EmployeeManagement.Api.Models
 
             return null;
         }
+
+        public async Task<IEnumerable<Employee>> Search(string name, Gender? gender)
+        {
+            IQueryable<Employee> query = appDbContext.Employees;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                query = query.Where(e => e.FirstName.Contains(name)
+                            || e.LastName.Contains(name));
+            }
+
+            if (gender != null)
+            {
+                query = query.Where(e => e.Gender == gender);
+            }
+
+            return await query.ToListAsync();
+        }
     }
 }
