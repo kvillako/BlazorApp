a081ede3f37b0b88a960abd100dffbb69459de04
kvillako <katlinvillako@gmail.com>
2021-03-31 10:55:50 +0300

    Part 30. Binding select element with database data in Blazor

--- BlazorTutorial/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
+++ BlazorTutorial/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -14,12 +14,24 @@ namespace EmployeeManagement.Web.Pages
         public IEmployeeService EmployeeService { get; set; }
         public Employee Employee { get; set; } = new Employee();
 
+        [Inject]
+        public IDepartmentService DepartmentService { get; set; }
+
+        public List<Department> Departments { get; set; } = new List<Department>();
+
+        public string DepartmentId { get; set; }
+
+
         [Parameter]
         public string Id { get; set; }
 
         protected async override Task OnInitializedAsync()
         {
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+            Departments = (await DepartmentService.GetDepartments()).ToList();
+            DepartmentId = Employee.DepartmentId.ToString();
+
+
         }
     }
 }
