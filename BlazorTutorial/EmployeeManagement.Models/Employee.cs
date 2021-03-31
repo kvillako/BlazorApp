db75d2ddef3a4252be94ec63eec19d56f601329f
kvillako <katlinvillako@gmail.com>
2021-03-31 12:29:31 +0300

    Part 35. Blazor form validation

--- BlazorTutorial/EmployeeManagement.Models/Employee.cs
+++ BlazorTutorial/EmployeeManagement.Models/Employee.cs
@@ -8,11 +8,12 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int EmployeeId { get; set; }
-        [Required]
-        [StringLength(100, MinimumLength = 2)]
+        [Required(ErrorMessage ="FirstName must be provided")]
+        [MinLength(2)]
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public DateTime DateOfBrith { get; set; }
         public Gender Gender { get; set; }
