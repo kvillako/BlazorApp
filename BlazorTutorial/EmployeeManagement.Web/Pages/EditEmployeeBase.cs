0d4bc9f923848ae92fbd3422ae4ae038e67ea592
kvillako <katlinvillako@gmail.com>
2021-04-05 11:56:05 +0300

    Part 40. Edit and Update in Blazor

--- BlazorTutorial/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
+++ BlazorTutorial/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -30,6 +30,9 @@ namespace EmployeeManagement.Web.Pages
         [Inject]
         public IMapper Mapper { get; set; }
 
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
         protected async override Task OnInitializedAsync()
         {
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
@@ -49,7 +52,15 @@ namespace EmployeeManagement.Web.Pages
             //EditEmployeeModel.Department = Employee.Department;
         }
 
-        protected void HandleValidSubmit()
-        { }
+        protected async Task HandleValidSubmit()
+        {
+            Mapper.Map(EditEmployeeModel, Employee);
+            var result = await EmployeeService.UpdateEmployee(Employee);
+
+            if (result != null)
+            {
+                NavigationManager.NavigateTo("/");
+            }
+        }
     }
 }
